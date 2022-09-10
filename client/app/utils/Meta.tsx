45e317fe2523ca232721efff9d1ec93467b7f00f
Magomed <magomed.demishtigov@yandex.ru>
2022-09-10 10:04:22 +0300

    added meta components

---
+++ client/app/utils/Meta.tsx
@@ -0,0 +1,29 @@
+import { Head } from 'next/document'
+import { FC } from 'react'
+
+interface IMeta {
+	title: string
+	description?: string
+}
+
+const Meta: FC<IMeta> = ({ title, description }) => {
+	return (
+		<>
+			<Head>
+				<title>{title}</title>
+				<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+				{description ? (
+					<meta
+						// itemProp="description"
+						name="description"
+						content={description}
+					/>
+				) : (
+					<meta name="robots" content="noindex, nofollow" />
+				)}
+			</Head>
+		</>
+	)
+}
+
+export default Meta
